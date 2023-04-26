1cea5a10dcd0ec1f2314170c9280da07e81245f5
Ramirods <ramiro.dasilva@correo.ucu.edu.uy>
2023-04-26 20:46:04 -0300

    Elfo listo

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,12 +6,15 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Enano enanoVirgo1  = new Enano("Tony", 100 , 20, 10); 
+            Enano enanoVirgo1  = new Enano("Tony", 100 , 0, 0); 
             Pony pony1 = new Pony("Facu", 10, 10);
             Mazo mazo1 = new Mazo("Rompedor", 40, 20);
-            Elfo elfo1 = new Elfo("Elfanfarron", 150, 35, 20);
+            Elfo elfo1 = new Elfo("Elfanfarron", 100, 0, 0);
             Escudo escudo1 = new Escudo("Muro", 10, 50);
             Varita varita1 = new Varita("Palito", 80, 10);
+            Mago mago1 = new Mago("Roberto", 100, 0, 0);
+            Escoba escoba1 = new Escoba("Barredora", 20, 10);
+            Baston baston1 = new Baston("Vencedor", 40, 20);
         }
     }
 }
