f4a84b7c3401320aec1ac01c616b1e8e7cfca42e
Ramirods <ramiro.dasilva@correo.ucu.edu.uy>
2023-04-26 20:37:02 -0300

    Elfo Listo

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,10 @@ namespace Program
         {
             Enano enanoVirgo1  = new Enano("Tony", 100 , 20, 10); 
             Pony pony1 = new Pony("Facu", 10, 10);
+            Mazo mazo1 = new Mazo("Rompedor", 40, 20);
+            Elfo elfo1 = new Elfo("Elfanfarron", 150, 35, 20);
+            Escudo escudo1 = new Escudo("Muro", 10, 50);
+            Varita varita1 = new Varita("Palito", 80, 10);
         }
     }
 }
