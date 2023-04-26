296fe6bbd25796486244a5d9330926615e00d317
Anthony Pereira <101676526+Clarensedor@users.noreply.github.com>
2023-04-26 19:42:27 -0300

    se crea el enano el mazo y el pony

---
+++ src/Library/Items/Mazo.cs
@@ -0,0 +1,27 @@
+namespace Program
+{
+   //Se declara la clase
+   public class Mazo {
+
+      //Se definen los atributos/propiedades
+      private string nombre;
+      private int ataque;
+      private int defensa;
+
+      //constructor
+      public Mazo(string nombre, int ataque, int defensa){
+         this.Nombre = nombre;
+         this.Ataque = ataque;
+         this.Defensa = defensa;
+      }
+
+      //getters and seters
+      public string Nombre { get; private set; }
+
+      public int Ataque { get; private set; }
+      public int Defensa { get; private set; }
+
+   }
+
+
+}
