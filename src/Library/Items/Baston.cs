2a0145c16ba6d1259d14550e7df06b3cda8ca638
PauAdjemian <paulaadjemian2004@gmail.com>
2023-05-10 20:14:56 -0300

    Mago/Bastón/Escoba

--- src/Library/Items/Baston.cs
+++ src/Library/Items/Baston.cs
@@ -1,8 +1,20 @@
 
 public class Baston
 {
-    private string Name {get; set; }
-    private int Ataque {get; set; }
-    private int Defensa{get; set; }
-    
+    //atributos
+    private string nombre {get; set; }
+    private int ataque {get; set; }
+    private int defensa{get; set; }
+    //constructor
+    public Baston(string nombre, int ataque, int defensa){
+         this.Nombre = nombre;
+         this.Ataque = ataque;
+         this.Defensa = defensa;
+      }
+
+      //getters and seters
+      public string Nombre { get; private set; }
+
+      public int Ataque { get; private set; }
+      public int Defensa { get; private set; }
 }
--- src/Library/Personajes/Mago.cs
+++ src/Library/Personajes/Mago.cs
@@ -2,15 +2,24 @@ using System;
 
 public class Mago
 {
-
-
+    private int vida=100;
     public Mago(string name)
     {
         this.Name = name;
     }
-    public string Name {get; set; }
+    public string Name{get; set; }
+    public int Vida{get; set; }
     public int Daño{get; set; }
     public int Defensa{get; set; }
 
+    public void RecibirDaño(int dañoRecibido)
+    {
+        this.Vida-=Vida-dañoRecibido;
+
+    }
+    public void Curarse()
+    {
+        this.Vida=100;
+    }
 
 }
