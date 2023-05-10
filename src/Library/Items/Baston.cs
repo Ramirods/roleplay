2a9fd3861139af08037fc9f5eb66cd9e27e4ca54
PauAdjemian <paulaadjemian2004@gmail.com>
2023-05-10 20:14:34 -0300

    Bastón/Mago/Escoba

---
+++ src/Library/Items/Baston.cs
@@ -0,0 +1,8 @@
+
+public class Baston
+{
+    private string Name {get; set; }
+    private int Ataque {get; set; }
+    private int Defensa{get; set; }
+    
+}
---
+++ src/Library/Personajes/Mago.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Mago
+{
+
+
+    public Mago(string name)
+    {
+        this.Name = name;
+    }
+    public string Name {get; set; }
+    public int Daño{get; set; }
+    public int Defensa{get; set; }
+
+
+}
