db0be1ea820a972965665ce4769208e84bb638a7
PauAdjemian <paulaadjemian2004@gmail.com>
2023-05-14 18:11:11 -0300

    Wizard/SpellBook

---
+++ src/Library/Items/Spell.cs
@@ -0,0 +1,19 @@
+namespace RoleplayGame;
+
+public class Spell : IMagic
+{
+    public int AttackValue
+    {
+        get
+        {
+            return 85;
+        }
+    }
+    public int DefenseValue
+    {
+        get
+        {
+            return 54;
+        }
+    }
+}
