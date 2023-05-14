d9aef9f1c32a1de4d797116413a316fed02c5ca9
PauAdjemian <paulaadjemian2004@gmail.com>
2023-05-14 17:00:29 -0300

    Mago

--- src/Library/Personajes/Mago.cs
+++ src/Library/Personajes/Mago.cs
@@ -4,18 +4,26 @@ namespace Program;
 public class Mago
 {
     private int vida=100;
+    private string name;
+    
+    private int daño;
+    private int defensa;
     public Mago(string name,int daño,int defensa)
     {
         this.Name = name;
+        this.Daño = daño;
+        this.Defensa = defensa;
     }
-    public string Name{get; set; }
-    public int Vida{get; set; }
-    public int Daño{get; set; }
-    public int Defensa{get; set; }
+    public string Name{ get{return this.name;}set{this.name = value;}}
+
+      public int Vida{get{return this.vida;}set{this.vida = value;}}
+
+      public int Daño{get{return this.daño;}set{this.daño = value;}}
+      public int Defensa{get{return this.defensa;}set{this.defensa = value;}}
 
     public void RecibirDaño(int dañoRecibido)
     {
-        this.Vida-=Vida-dañoRecibido;
+         this.vida = this.vida - (dañoRecibido - this.defensa);
 
     }
     public void Curarse()
